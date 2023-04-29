4503e679e3a965897af20b7ac70edffadeb34f94
Suman Balayar <076bei042.suman@pcampus.edu.np>
2023-04-29 18:44:33 +0545

    frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import "./App.css";
 //import { Link } from 'react-router';
-
+import Nav from "./components/Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-     <p>Hello this is me</p>
+      <BrowserRouter>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component</h1>}/>
+          <Route path="/add" element={<h1>Product Add Component</h1>} />
+          <Route path="/update" element={<h1>Product Update Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3>
+        Ecommerce Dashboard Solution to Manage all your ecommerce marketplace.
+      </h3>
+    </div>
+  );
+};
+
+export default Footer;
